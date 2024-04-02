f390c902a30acb262e5e09dcf962cd6a06e4b2e7
ShellSquad <131351541+ShellSquad@users.noreply.github.com>
2024-04-02 15:47:10 +0530

    Update settings.py
    naam naikhu che

--- food_app/settings.py
+++ food_app/settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+
+jay chutiya
+
 """
 
 from pathlib import Path
